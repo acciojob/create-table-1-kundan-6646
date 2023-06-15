8414c7e6c492a2e981a1f628df9a05f120f8623f
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-06-15 15:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const table = document.getElementsByTagName('table')[0];
-let currRows = 2;
 function insert_Row() {
-	currRows++;
 	let row = document.createElement('tr');
-    row.innerHTML =  `<td>Row${currRows} cell1</td> 
-		<td>Row${currRows} cell2</td>`;
-    table.append(row);
+    row.innerHTML =  `<td>New Cell1</td> 
+		<td>New Cell2</td>`;
+    table.prepend(row);
 }
